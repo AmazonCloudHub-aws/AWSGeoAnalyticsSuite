e683b4186111b9397e8040b812acaf4cc2084d3d
tutorial <robinvp3@gmail.com>
2024-02-16 15:38:39 +0100

    add others

---
+++ .history/tfvars_20240216153721.tf
@@ -0,0 +1,9 @@
+
+
+subnet_id                = "subnet-12345abcd"
+master_sg_id             = "sg-12345abcd"
+slave_sg_id              = "sg-abcde12345"
+redshift_master_password = "YourSecurePassword1"
+
+
+Provider.tf
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+
+subnet_id                = "subnet-12345abcd"
+master_sg_id             = "sg-12345abcd"
+slave_sg_id              = "sg-abcde12345"
+redshift_master_password = "YourSecurePassword1"
