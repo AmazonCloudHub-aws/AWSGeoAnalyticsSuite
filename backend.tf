2ed069c235c56cc2f02ea97486098df1f4fc88d3
dhamsey3 <73405591+dhamsey3@users.noreply.github.com>
2024-04-18 21:11:55 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -10,3 +10,30 @@ terraform {
   }
 }
 
+## Backend bucket
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-backend-bucket"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform Backend Bucket"
+    Environment = "Production"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.terraform_state.bucket
+}
